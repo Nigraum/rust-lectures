f20917daefa64bcabbd910fbecf7bf5bfade3f3c
Nigraum <yan.corintima@hotmail.com>
2022-10-04 20:10:52 -0300

    Cmpile tutorial project

---
+++ cargo_test/tutorial/target/debug/deps/tutorial-6f9a68847a291329.d
@@ -0,0 +1,5 @@
+/home/nigraum/workspace/rust/cargo_test/tutorial/target/debug/deps/tutorial-6f9a68847a291329: src/main.rs
+
+/home/nigraum/workspace/rust/cargo_test/tutorial/target/debug/deps/tutorial-6f9a68847a291329.d: src/main.rs
+
+src/main.rs:
---
+++ cargo_test/tutorial/target/debug/tutorial.d
@@ -0,0 +1,1 @@
+/home/nigraum/workspace/rust/cargo_test/tutorial/target/debug/tutorial: /home/nigraum/workspace/rust/cargo_test/tutorial/src/main.rs
